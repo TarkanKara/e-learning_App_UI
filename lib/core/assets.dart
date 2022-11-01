51307a0d1d87f2a0a5da640ca5036c7613e7daf8
Tarkan Kara <unnahmet23@hotmail.com>
2022-11-01 18:50:15 +0300

    e_learning project setup

---
+++ lib/core/assets.dart
@@ -0,0 +1,12 @@
+class AssetsApp {
+  //assets/images
+  static const String welcome = "assets/images/welcome.png";
+  static const String ux = "assets/images/ux.png";
+  static const String programming = "assets/images/programming.png";
+  static const String profile = "assets/images/profile.png";
+  static const String graphics = "assets/images/graphics.png";
+  static const String finance = "assets/images/finance.png";
+
+  //assets/icons
+  static const String icon = "assets/icons/sort.png";
+}
---
+++ lib/core/color_constats.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+
+class ColorClass {
+  static const Color cpink = Color(0xFFff6374);
+  static const Color cblue = Color(0xFF71b8ff);
+  static const Color cpurple = Color(0xFF9ba0fc);
+  static const Color corange = Color(0xFFffaa5b);
+}
