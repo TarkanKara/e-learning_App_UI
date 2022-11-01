41479a9c5e73e98887127d45b6771d8080432c95
Tarkan Kara <unnahmet23@hotmail.com>
2022-11-01 21:41:18 +0300

    Created home_page.dart and Updated

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,14 +1,188 @@
+import 'package:e_learning_app/core/assets.dart';
+import 'package:e_learning_app/core/color_constats.dart';
+import 'package:e_learning_app/models/product_model.dart';
+import 'package:e_learning_app/widgets/custome_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final List<String> category = ["Top", "Design", "Marketin", "Programing"];
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: const [],
+          children: [
+            const CustomeAppBar(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Hi Tarkan",
+                        style: Theme.of(context).textTheme.headline2!.copyWith(
+                              fontSize: 25,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "Today is a good day\n to learn something new!",
+                        style: Theme.of(context).textTheme.headline2!.copyWith(
+                              fontSize: 15,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: const BoxDecoration(
+                          color: ColorClass.corange,
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                        child: Image.asset(
+                          AssetsApp.profile,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 55,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: category.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.all(5),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          decoration: const BoxDecoration(
+                            color: ColorClass.corange,
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          child: Text(
+                            category[index],
+                            style:
+                                Theme.of(context).textTheme.bodyText1!.copyWith(
+                                      fontSize: 18,
+                                      color: Colors.red,
+                                    ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Categories",
+                    style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "See All",
+                      style:
+                          Theme.of(context).textTheme.headlineMedium!.copyWith(
+                                fontSize: 20,
+                                color: ColorClass.cblue,
+                                fontWeight: FontWeight.bold,
+                              ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              itemCount: products.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.85,
+              ),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Container(
+                    height: 80,
+                    decoration: BoxDecoration(
+                      color: products[index].color,
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 120,
+                          child: Image.asset(
+                            products[index].image,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          products[index].title,
+                          textAlign: TextAlign.center,
+                          style:
+                              Theme.of(context).textTheme.labelMedium!.copyWith(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                  ),
+                        ),
+                        Text(
+                          "${products[index].courses.toString()} courses",
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .copyWith(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            )
+          ],
         ),
       ),
     );
